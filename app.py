e855858e34f8900e0e0431069e6524662b2ed4b3
Sarim <108764082+HydraulicSponge@users.noreply.github.com>
2024-03-21 17:32:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ classes = ['healthy', 'mild npdr', 'moderate npdr', 'severe npdr', 'pdr']
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
-    #transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 model.eval()
 
