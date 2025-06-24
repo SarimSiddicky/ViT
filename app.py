a07ba3512197232c166e3f5cbc7a1bd7d5a83c3f
Sarim <108764082+SarimSiddicky@users.noreply.github.com>
2025-06-24 15:17:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,25 @@
 import gradio as gr
-import tkinter as tk
-from tkinter import filedialog
 from PIL import Image
 import torch
 import torchvision.transforms as transforms
+import torchvision.models as models
+import torch.nn as nn
 
-# Load the model
-model = torch.load('model_architecture (4).pth',map_location=torch.device('cpu'))
-model.load_state_dict(torch.load('model_weights (4).pth',map_location=torch.device('cpu')))
+
+model = models.vit_b_16(weights=None)
+model.heads = nn.Sequential(nn.Linear(768, 5)) 
+
+weights_path = "best.pth"
+model.load_state_dict(torch.load(weights_path, map_location='cpu'))
 model.eval()
 
-# Define the classes (replace with your actual class names)
 classes = ['healthy', 'mild npdr', 'moderate npdr', 'severe npdr', 'pdr']
 
-# Define the image transformation
 transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+transforms.Resize((224, 224)),
+transforms.ToTensor(),
+transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
-model.eval()
 
 def classify_image(image):
     image_tensor = transform(image).unsqueeze(0)
@@ -28,5 +28,5 @@ def classify_image(image):
         probabilities = torch.nn.functional.softmax(outputs, dim=1)
     return {classes[i]: probabilities[0][i].item() for i in range(len(classes))}
 
-# Launch the Gradio interface
+# Main
 gr.Interface(fn=classify_image, inputs=gr.Image(type="pil"), outputs=gr.Label(num_top_classes=5), title="Diabetic Retinopathy Classifier").launch()
